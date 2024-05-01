c81db073b61ed9005b085de3fbb1a44d3db439be
Akira Kono <a.dilla.kono@gmail.com>
2024-05-01 12:37:33 +0900

    feat(api): Add post pet API

--- lambdas/api/index.ts
+++ lambdas/api/index.ts
@@ -1,21 +1,32 @@
 import * as v from "valibot";
-import { PetsSchema } from "@soracom/my-model";
+import { PetSchema, PetsSchema } from "@soracom/my-model";
 import { Router } from "@soracom/my-valibot-router";
 import { hello } from "@internal/util";
 
 const r = new Router();
 
-r.addRoute(new RegExp("/pets"), undefined).addHandler({
-  method: "get",
-  queryParams: v.optional(
-    v.object({
-      limit: v.number([v.integer(), v.maxValue(100)]),
-    })
-  ),
-  response: PetsSchema,
-  handler: async (params) => ({
-    statusCode: 200,
-    headers: { hello: hello() },
-    body: [],
-  }),
-});
+r.addRoute(new RegExp("/pets"), undefined)
+  .addHandler({
+    method: "get",
+    queryParams: v.optional(
+      v.object({
+        limit: v.number([v.integer(), v.maxValue(100)]),
+      })
+    ),
+    response: PetsSchema,
+    handler: async (params) => ({
+      statusCode: 200,
+      headers: { hello: hello() },
+      body: [],
+    }),
+  })
+  .addHandler({
+    method: "post",
+    bodyParams: PetSchema,
+    response: PetSchema,
+    handler: async (params) => ({
+      statusCode: 204,
+      headers: {},
+      body: params,
+    }),
+  });
